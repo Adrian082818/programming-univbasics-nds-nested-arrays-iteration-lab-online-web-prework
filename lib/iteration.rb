c32c6f449256f14104418b6ea3f0e64dae7f2cd5
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-10 13:42:53 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -23,9 +23,9 @@ def find_greater_pair(src)
 while row_index < src.count do 
   element_index = 0 
   while element_index < src[row_index].count do
-    total += src[row_index][element_index].sort
-    row_index += 1 
-    element_index += 1 
+    total += src[row_index][element_index]
+   element_index += 1 
+   row_index += 1 
 end
 return total
 end 

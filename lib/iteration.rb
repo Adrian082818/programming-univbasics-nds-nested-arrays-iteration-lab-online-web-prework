26a7c77a15b32f69d74b0b73bca1e774981f1222
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-10 13:39:38 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,13 +6,29 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
+  results = []
+  row_index = 0 
+  while row_index < src.count do
+    current_items = src[row_index]
+    pizza_sentence = "I love #{current_items[0]} and #{current_items[1]} on my pizza"
+    results << pizza_sentence
+    row_index += 1
+end 
+return results 
+end 
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  outer_results = []
+row_index = 0 
+while row_index < src.count do 
+  element_index = 0 
+  while element_index < src[row_index].count do
+    row_index += 1 
+    element_index += 1 
 end
+return outer_results
+end 
+end 
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
@@ -22,4 +38,4 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-end
+ end

a1e9cfe6be818a7fbbc61721063dc696314e27d4
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-10 14:01:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,7 +45,7 @@ def total_even_pairs(src)
   while row_index < src.count do 
     element_index = 0 
     while element_index < src[row_index].count do 
-      total == src[row_index][element_index]
+      total += src[row_index][element_index]
     element_index += 1 
     row_index += 1 
     results << total
